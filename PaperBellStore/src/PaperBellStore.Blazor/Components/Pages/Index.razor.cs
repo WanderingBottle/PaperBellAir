e5317e33d675b80a7410be51d5b1d04d991a3b92
rm.tang <rm.tang@agvsz.com>
2025-10-29 10:12:54 +0800

    fix(启动显示页面):修改启动显示页面自动到登录页

--- PaperBellStore/src/PaperBellStore.Blazor/Components/Pages/Index.razor.cs
+++ PaperBellStore/src/PaperBellStore.Blazor/Components/Pages/Index.razor.cs
@@ -8,6 +8,16 @@ public partial class Index
     [Inject]
     protected NavigationManager Navigation { get; set; } = default!;
 
+    protected override void OnInitialized()
+    {
+        // 如果用户未登录,直接重定向到登录页面
+        if (!CurrentUser.IsAuthenticated)
+        {
+            Navigation.NavigateTo("/Account/Login", forceLoad: true);
+        }
+        base.OnInitialized();
+    }
+
     private void Login()
     {
         Navigation.NavigateTo("/Account/Login", true);
