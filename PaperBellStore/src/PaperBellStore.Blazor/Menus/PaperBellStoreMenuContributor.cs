3055fea5a5f9f379572233678442fe315906cd2c
rm.tang <rm.tang@agvsz.com>
2025-11-04 10:46:37 +0800

    fix(项目管理):修改项目管理的页面是二级菜单以及页面的面包屑导航格式使用js脚本注入实现原生的面包屑功能

--- PaperBellStore/src/PaperBellStore.Blazor/Menus/PaperBellStoreMenuContributor.cs
+++ PaperBellStore/src/PaperBellStore.Blazor/Menus/PaperBellStoreMenuContributor.cs
@@ -16,7 +16,7 @@ public class PaperBellStoreMenuContributor : IMenuContributor
 {
     public async Task ConfigureMenuAsync(MenuConfigurationContext context)
     {
-        if(context.Menu.Name==StandardMenus.Main)
+        if (context.Menu.Name == StandardMenus.Main)
         {
             await ConfigureMainMenuAsync(context);
         }
@@ -28,42 +28,53 @@ public class PaperBellStoreMenuContributor : IMenuContributor
         var projectL = context.GetLocalizer<ProjectManageResource>();
 
         context.Menu.Items.Insert(
-            0 ,
+            0,
             new ApplicationMenuItem(
-                PaperBellStoreMenus.Home ,
-                l["Menu:Home"] ,
-                "/" ,
-                icon: "fas fa-home" ,
+                PaperBellStoreMenus.Home,
+                l["Menu:Home"],
+                "/",
+                icon: "fas fa-home",
                 order: 1
             )
         );
 
-        context.Menu.Items.Insert(
-            1 ,
+        // 创建项目管理父菜单
+        var projectManagementMenu = new ApplicationMenuItem(
+            "ProjectManagement",
+            projectL["Menu:ProjectManagement"],
+            null, // 父菜单不设置 URL
+            icon: "fas fa-project-diagram",
+            order: 2
+        );
+
+        // 添加子菜单项：项目列表
+        projectManagementMenu.AddItem(
             new ApplicationMenuItem(
-                "ProjectManagement" ,
-                projectL["Menu:ProjectManagement"] ,
-                "/projects" ,
-                icon: "fas fa-project-diagram" ,
-                order: 2
+                "ProjectManagement.Projects",
+                projectL["Menu:ProjectManagement.Projects"],
+                "/projects",
+                icon: "fas fa-list",
+                order: 1
             )
         );
 
+        context.Menu.Items.Insert(1, projectManagementMenu);
+
         //Administration
         var administration = context.Menu.GetAdministration();
-        administration.Order=6;
+        administration.Order = 6;
 
-        if(MultiTenancyConsts.IsEnabled)
+        if (MultiTenancyConsts.IsEnabled)
         {
-            administration.SetSubItemOrder(TenantManagementMenuNames.GroupName , 1);
+            administration.SetSubItemOrder(TenantManagementMenuNames.GroupName, 1);
         }
         else
         {
             administration.TryRemoveMenuItem(TenantManagementMenuNames.GroupName);
         }
 
-        administration.SetSubItemOrder(IdentityMenuNames.GroupName , 2);
-        administration.SetSubItemOrder(SettingManagementMenus.GroupName , 3);
+        administration.SetSubItemOrder(IdentityMenuNames.GroupName, 2);
+        administration.SetSubItemOrder(SettingManagementMenus.GroupName, 3);
 
         return Task.CompletedTask;
     }
